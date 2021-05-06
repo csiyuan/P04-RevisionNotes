c435bc566bc9ecb4f6d19ba777668c848e132334
19045104 <19045104@myrp.edu.sg>
2021-05-06 15:24:13 +0800

    update

--- app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/SecondActivity.java
+++ app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/SecondActivity.java
@@ -1,14 +1,26 @@
 package com.myapplicationdev.android.p04_revisionnotes;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class SecondActivity extends AppCompatActivity {
+	ListView lv;
+	ArrayAdapter aa;
+	ArrayList<Note> note;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		//TODO implement the Custom ListView
+
+		lv = findViewById(R.id.lv);
+
+
 
 	}
 
