24e57ae779b2d01831878fce19d87dc42fe4d6c7
19016011 <19016011@rp.edu.sg>
2021-05-06 14:05:30 +0800

    Commit

--- app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/DBHelper.java
+++ app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/DBHelper.java
@@ -12,8 +12,8 @@ import java.util.ArrayList;
 public class DBHelper extends SQLiteOpenHelper {
 
 	//TODO Define the Database properties
-	private static final String DATABASE_NAME = "";
-	private static final int DATABASE_VERSION = ;
+	private static final String DATABASE_NAME = "Notes.db";
+	private static final int DATABASE_VER = 1;
 
 	private static final String TABLE_NOTE = "note";
 	private static final String COLUMN_ID = "id";
@@ -22,7 +22,7 @@ public class DBHelper extends SQLiteOpenHelper {
 
 
 	public DBHelper(Context context) {
-		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		super(context, DATABASE_NAME, null, DATABASE_VER);
 	}
 
 	@Override
